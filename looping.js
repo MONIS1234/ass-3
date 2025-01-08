f36dc10eb9fe90f611dddd72f09c69f5a9b3df9f
MONIS1234 <2211cs010502@mallareddyuniversity.ac.in>
2025-01-08 13:35:37 +0530

    newfile

---
+++ looping.js
@@ -0,0 +1,27 @@
+for(let i=1; i<=10;i++){
+    console.log("Javascript");
+}
+
+//1 is odd
+//2is even
+for(let i=1;i<=100;i++){
+    if(i%2 !=0){
+        console.log(i,"odd");
+
+    }else{
+        console.log(i,"even");
+
+    }
+    
+}
+
+
+let a=parseInt(prompt("Enter the number :"))
+let s=1;
+for(a;a >= 1;a--){
+    s= s*a
+    
+}
+console.log(s);
+
+//wap to find factors of a numbers : 12,-2,3,4,6
